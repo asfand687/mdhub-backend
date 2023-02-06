4890536664410f0f8b38c89a14d9286f27be43c9
asfand687 <asfandyar687@gmail.com>
2023-02-07 02:26:39 +0500

    add boilerplate for express

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import connectDatabase from './mongodb/connect'
+
+dotenv.config()
+
+const app = express()
+
+// middleware
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded())
+app.use(cors())
+
+
+app.get('/', async (req, res) => {
+  res.send("Hello from MD Hub")
+})
+
+const startServer = async () => {
+  try {
+    connectDatabase(process.env.MONGODB_URI)
+    app.listen(8080, () => {
+      console.log("Server has started on port: http://localhost:8080")
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
