92fa2486db39724b247f0004194676a60a263709
asfand687 <asfandyar687@gmail.com>
2023-02-07 03:27:05 +0500

    added middleware route for auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import connectDatabase from './mongodb/connect.js'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 
@@ -11,7 +12,7 @@ const app = express()
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
-
+app.use('/api/v1/auth', authRoutes)
 
 app.get('/', async (req, res) => {
   res.send("Hello from MD Hub")
