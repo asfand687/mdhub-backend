f183ee923af6ab2891552eb153562017fa1d2d5c
asfand687 <asfandyar687@gmail.com>
2023-02-07 11:32:13 +0500

    added userLogin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 
 export const registerUser = async (req, res) => {
@@ -19,5 +20,35 @@ export const registerUser = async (req, res) => {
   }
 }
 
+export const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne(
+      {
+        email: req.body.email
+      }
+    );
+
+    !user && res.status(401).json("User not found");
+
+    await bcrypt.compare(req.body.password, user.password, function (err, result) {
+      if (result) {
+        const accessToken = jwt.sign(
+          {
+            id: user._id,
+            isAdmin: user.isAdmin,
+          },
+          process.env.JWT_SEC,
+          { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+      }
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+}
 
 
