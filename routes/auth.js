0f436b026dd213d277d0442d598e3dc2184acb3f
asfand687 <asfandyar687@gmail.com>
2023-02-07 11:32:22 +0500

    added login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { registerUser } from '../controllers/authController.js'
+import { registerUser, loginUser } from '../controllers/authController.js'
  
 const router = express.Router()
 
 //REGISTER
 router.route('/register').post(registerUser)
+
+// LOGIN
+router.route('/login').post(loginUser)
 
 export default router
