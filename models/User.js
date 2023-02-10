4da7c487a0808500a7c259611f2e8956f30d13fb
asfand687 <asfandyar687@gmail.com>
2023-02-10 21:09:02 +0500

    added additional fields to user model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,13 @@ const User = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique : true },
   password: { type: String, required: true, unique : true },
-  phone: { type: String, required: true, unique: true}
+  phone: { type: String, required: true, unique: true },
+  isChildUser: { type: Boolean, default: false },
+  address: { type: String },
+  city: { type: String },
+  province: { type: String },
+  postalCode: { type: String },
+  childAccounts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChildAccount' }]
 }, 
 { timestamps: true }
 )
