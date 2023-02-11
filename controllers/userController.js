2d3166d7d545065fe52f56a103a8ea98bb34178c
asfand687 <asfandyar687@gmail.com>
2023-02-11 23:34:03 +0500

    added get user

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+import User from "../models/User.js"
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
