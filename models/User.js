005ce5ed3dcfbb9ed5fd3bd4b40acf2c06e01858
asfand687 <asfandyar687@gmail.com>
2023-02-15 23:27:50 +0500

    removed required attribute from password

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const User = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique : true },
-  password: { type: String, required: true, unique : true },
+  password: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
   isChildUser: { type: Boolean, default: false },
   address: { type: String },
