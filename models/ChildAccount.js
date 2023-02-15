1a55d7dfb305d5835d9936c922475f6169df5f1f
asfand687 <asfandyar687@gmail.com>
2023-02-15 23:27:34 +0500

    removed unique attributes

--- models/ChildAccount.js
+++ models/ChildAccount.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const ChildAccount = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
-  phone: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+  password: { type: String },
+  phone: { type: String, required: true },
   isChildUser: { type: Boolean, default: true },
   parentAccountId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
 },
