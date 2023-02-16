70fb3e2d362c9a83fb1294c1f513e8aa922282c1
asfand687 <asfandyar687@gmail.com>
2023-02-16 23:58:05 +0500

    added user update route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { verifyToken } from '../utils/utils.js'
-import { getUser } from '../controllers/userController.js'
+import { getUser, updateUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 //Get User
 router.route('/:id').get(verifyToken, getUser)
+
+// Update User
+router.route("/:id").put(verifyToken, updateUser)
 
 
 
