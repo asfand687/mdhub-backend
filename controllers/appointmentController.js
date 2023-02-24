0abf1dbe9287efee572c6ca0b330c920c9222b50
asfand687 <asfandyar687@gmail.com>
2023-02-24 23:14:20 +0500

    added confirm appointment

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,20 +1,27 @@
 import Appointment from '../models/Appointment.js'
 import User from "../models/User.js"
+import { confirmAppointmentPaymentIntent } from '../utils/utils.js'
 
 export const createAppointment = async (req, res) => {
-  const { date, userId, service } = req.body
+  const { date, customerId, service, time, additionalInfo, paymentMethod, amount, userId } = req.body
   try {
-    const newAppointment = await new Appointment({
-      date,
-      userId,
-      service
-    })
-    await newAppointment.save()
-    const user = await User.findOne({ _id: userId })
-    await user.appointments.push(newAppointment._id)
-    await user.save()
-    await newAppointment.save()
-    res.status(201).json({ success: true, message: 'Appointment added successfully' })
+    await confirmAppointmentPaymentIntent(req, customerId, paymentMethod, amount)
+    if (confirmAppointmentPaymentIntent) {
+      const newAppointment = await new Appointment({
+        date,
+        userId,
+        service,
+        time,
+        additionalInfo,
+        amount
+      })
+      await newAppointment.save()
+      const user = await User.findOne({ _id: userId })
+      user.appointments.push(newAppointment._id)
+      await user.save()
+      await newAppointment.save()
+      res.status(201).json({ success: true, message: 'Appointment added successfully' })
+    }
   } catch (error) {
     console.log(error.message)
     res.status(500).json({ success: false, message: 'Unable to add appointment' });
