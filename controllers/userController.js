4aa4fb1ba87684024f3db49bf98e5ac56f66dd9e
asfand687 <asfandyar687@gmail.com>
2023-02-24 23:15:23 +0500

    added updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,25 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
+import { getPaymentInfo, updatePaymentMethod } from "../utils/utils.js"
 
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id).populate("childAccounts");
+    const paymentInfo = await getPaymentInfo(user.stripeCustomerId)
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    res.status(200).json({ paymentInfo, ...others });
   } catch (error) {
     res.status(500).json(error);
   }
 }
 
 export const updateUser = async (req, res) => {
-  if (req.body.password) {
-    req.body.password = bcrypt.hashSync(req.body.password, 10)
-  }
   try {
+    if (req.body.paymentMethod) {
+      const attachedPaymentMethod = await updatePaymentMethod(req.body.customerId, req.body.paymentMethod)
+      if (attachedPaymentMethod)
+        return res.status(200).json("Payment Method Updated")
+    }
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
