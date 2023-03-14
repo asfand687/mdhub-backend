ae160545372bbffeb799360168c23b1aa1c9eb6b
asfand687 <asfandyar687@gmail.com>
2023-03-15 02:27:30 +0500

    added admin routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,25 @@
 import express from 'express'
 import { verifyToken } from '../utils/utils.js'
-import { getUser, updateUser } from '../controllers/userController.js'
+import { getUser, updateUser, getAllUsers, deleteUser, getNewUserAndDeletedUserData, getUsersWithLatestPayment } from '../controllers/userController.js'
 
 const router = express.Router()
 
+// Get Users PaymentInfo
+router.route('/get_users_payment_info').get(getUsersWithLatestPayment)
+
+// Get Users Info and Deleted Users Info
+router.route('/get_user_info').get(getNewUserAndDeletedUserData)
+
 //Get User
 router.route('/:id').get(verifyToken, getUser)
+
+//Get All Users
+router.route('/').get(getAllUsers)
+
+//Delete User
+router.route('/:id').delete(deleteUser)
+
+
 
 // Update User
 router.route("/:id").put(verifyToken, updateUser)
