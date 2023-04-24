98cdd62dea06064efe508c330caf2653789a6564
Amir A <amir-azimi@hotmail.com>
2023-04-25 04:49:17 +0600

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -321,9 +321,9 @@ export const createStripeCustomer = async (req) => {
       description: "test customer for mdhub family monthly package",
       email: email,
       name: name,
-      payment_method: paymentMethod.id,
+      payment_method: paymentMethod,
       invoice_settings: {
-        default_payment_method: paymentMethod.id
+        default_payment_method: paymentMethod
       }
     })
     console.log('Customer created:', customer.id)
